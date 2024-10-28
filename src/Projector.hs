b3a33c7015d7ea2e37747ed3058ae702528ed825
Marcelo Lazaroni <lazaronijunior@gmail.com>
2024-10-28 15:28:28 +0000

    Add Projector file

---
+++ src/Projector.hs
@@ -0,0 +1,11 @@
+module Projector where
+
+{-| A projector reads messages from multiple queues, applies a filter to the
+stream and submits passing messages to a single data destination.
+
+We do not implement filters for now.
+-}
+
+project :: IO ()
+project = return ()
+
