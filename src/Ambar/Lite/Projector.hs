67f20ac709951f12107b60eb812d68e0c4e6d8ed
Marcelo Lazaroni <lazaronijunior@gmail.com>
2024-10-28 16:07:54 +0000

    Add Transport and HTTP transport

--- src/Ambar/Lite/Projector.hs
+++ src/Ambar/Lite/Projector.hs
@@ -5,7 +5,12 @@ stream and submits passing messages to a single data destination.
 
 We do not implement filters for now.
 -}
+import Ambar.Lite.Queue (Queue)
+import Ambar.Transport (Transport)
+import Utils.Some (Some)
 
-project :: IO ()
-project = return ()
+newtype Parallelism = Parallelism Int
+
+project :: Queue -> Some Transport -> Parallelism -> IO ()
+project = undefined
 
---
+++ src/Ambar/Transport.hs
@@ -0,0 +1,24 @@
+module Ambar.Transport where
+
+import Data.Text (Text)
+import Data.ByteString (ByteString)
+
+import Utils.Some (Some(..))
+
+-- | A transport is a way to send data to some destination.
+class Transport a where
+  send :: a -> ByteString -> IO (Maybe SubmissionError)
+
+  -- | Optional specialised function to allow json manipulation.
+  -- The default implementation just sends it as a bytes string.
+  sendJSON :: a -> ByteString -> IO (Maybe SubmissionError)
+  sendJSON = send
+
+data SubmissionError
+  = UnableToSend Text
+  | ClientError Text
+  deriving Show
+
+instance Transport (Some Transport) where
+  send (Some a) = send a
+  sendJSON (Some a) = sendJSON a
