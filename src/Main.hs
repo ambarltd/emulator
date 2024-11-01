63d3515327eb3342bb6ed564b0017552b9bb27bc
Marcelo Lazaroni <lazaronijunior@gmail.com>
2024-11-01 19:32:13 +0000

    Parse CLI options

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,52 @@
+{-# LANGUAGE ApplicativeDo #-}
 module Main where
 
+import Control.Applicative (optional)
+import Data.Maybe (fromMaybe)
 import qualified Options.Applicative as O
 import qualified Options.Applicative.Help.Pretty as OP
+import System.Directory (createDirectoryIfMissing, getXdgDirectory, XdgDirectory(..))
+import System.FilePath ((</>))
+
+import Ambar.Emulator (emulate)
+import Ambar.Emulator.Config (parseEnvConfigFile, EmulatorConfig(..))
+import Utils.Logger (plainLogger, Severity(..))
+
+_DEFAULT_PARTITIONS_PER_TOPIC :: Int
+_DEFAULT_PARTITIONS_PER_TOPIC = 10
 
 main :: IO ()
 main = do
-  Options <- O.execParser cliOptions
-  putStrLn "Done"
+  cmd <- O.execParser cliOptions
+  case cmd of
+    CmdRun{..} -> do
+      env <- parseEnvConfigFile o_configPath
+      queue <- maybe defaultQueuePath return o_queuePath
+      let config = EmulatorConfig
+            { c_partitionsPerTopic = fromMaybe _DEFAULT_PARTITIONS_PER_TOPIC o_partitionsPerTopic
+            , c_maxParallelism = Nothing -- can't be set for now
+            , c_queuePath = queue
+            }
+          severity = if o_verbose then Debug else Info
+      emulate (plainLogger severity) config env
 
-data Options = Options
+defaultQueuePath :: IO FilePath
+defaultQueuePath = do
+  -- here's some discussion why history files belong in XDG_DATA_HOME:
+  --   https://github.com/fish-shell/fish-shell/issues/744
+  dir <- getXdgDirectory XdgData "haskell-docs-cli"
+  createDirectoryIfMissing True dir
+  return (dir </> "haskell-docs-cli.history")
 
-cliOptions :: O.ParserInfo Options
+data Command
+  = CmdRun
+    { o_partitionsPerTopic :: Maybe Int
+    , o_queuePath :: Maybe FilePath
+    , o_configPath :: FilePath
+    , o_verbose :: Bool
+    }
+
+cliOptions :: O.ParserInfo Command
 cliOptions = O.info (O.helper <*> parser) $ mconcat
   [ O.fullDesc
   , O.headerDoc $ Just $ OP.vcat
@@ -26,6 +62,32 @@ cliOptions = O.info (O.helper <*> parser) $ mconcat
       <> OP.line
   ]
   where
-    parser = pure Options
+    parser = O.subparser $ mconcat
+      [ O.command "run"
+        $ O.info (O.helper <*> parserRun)
+        $ O.progDesc "run the emulator"
+      ]
+
+    parserRun = do
+      o_partitionsPerTopic <- optional $ O.option O.auto $ mconcat
+          [ O.long "partitions-per-topic"
+          , O.metavar "INT"
+          , O.help "How many partitions should newly created topics have."
+          ]
+      o_queuePath <- optional $ O.strOption $ mconcat
+          [ O.long "data-path"
+          , O.metavar "PATH"
+          , O.help "Where to put emulation data including file queues. Defaults to $XDG_DATA_HOME/ambar-emulator."
+          ]
+      o_configPath <- O.strOption $ mconcat
+          [ O.long "config"
+          , O.metavar "FILE"
+          , O.help "Yaml file with environment configuration. Spec at at <https://github.com/ambarltd/emulator>."
+          ]
+      o_verbose <- O.switch $ mconcat
+          [ O.long "verbose"
+          , O.help "Enable verbose logging."
+          ]
+      return CmdRun{..}
 
 
